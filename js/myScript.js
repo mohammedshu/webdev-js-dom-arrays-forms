1245a55eadea84a9cc13dc5cefaa346741c1ca58
mohammedshu <c1024633@my.shu.ac.uk>
2021-11-11 15:25:57 +0000

    name change

---
+++ js/myScript.js
@@ -0,0 +1,6 @@
+(function() {
+    //start
+    document.getElementById("myHeading").innerHTML = "Mohammed";
+    document.querySelector("nav ul li").setAttribute("class", "currentPage");
+    //end
+})();
