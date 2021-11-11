0c4aa208ee09e41b2651a709b562fcd6b3f06fdd
mohammedshu <c1024633@my.shu.ac.uk>
2021-11-11 15:54:57 +0000

    comments

--- js/myScript.js
+++ js/myScript.js
@@ -2,5 +2,20 @@
     //start
     document.getElementById("myHeading").innerHTML = "Mohammed";
     document.querySelector("nav ul li").setAttribute("class", "currentPage");
+    document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk");
+
+    // DOM event
+  //  document.getElementById('myTestEvent').onclick = myFunction;
+//function myFunction(){
+//console.info('hi');
+//}
+
+// addEventListener
+document.getElementById('myTestEvent').addEventListener('click',
+function(ev){
+console.dir(ev);
+});
+
+
     //end
 })();
